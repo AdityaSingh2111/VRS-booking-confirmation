42946beb2c6211859ce0b8068f2e083c29e43299
Aditya <adityaks2111@gmail.com>
2026-08-03 13:23:40 +0530

    resolving test

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+
     tanstackStart({
-      server: { entry: "server" },
+      server: {
+        entry: "server",
+      },
     }),
+
+    nitro(),
+
     react(),
   ],
+
   resolve: {
     tsconfigPaths: true,
     dedupe: ["react", "react-dom", "@tanstack/react-router"],
